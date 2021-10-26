5d576b81545437642adef6d32cbf8b38729fef2a
Seneca <you@example.com>
2021-10-25 23:00:17 -0400

    most of the tests will continue tomorrow night

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -18,8 +18,30 @@ public class PositionType {
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
+    @Override
+    public String toString() {
+        return value;
+    }
+
+
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PositionType that = (PositionType) o;
+
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
 
     // Getters and Setters:
 
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,12 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import javax.print.DocFlavor;
 
 /**
  * Created by LaunchCode
@@ -9,4 +14,39 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+    @Test
+    public void testSettingJobId()
+    {
+        Job jobOne = new Job();
+        Job jobTwo = new Job();
+        Assert.assertNotEquals(jobOne.getId(),jobTwo.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        Assert.assertTrue(testJob instanceof Job);
+
+        Assert.assertTrue(testJob.getName() == "Product tester");
+
+        Assert.assertTrue(testJob.getEmployer() instanceof Employer);
+        Assert.assertTrue(testJob.getEmployer().getValue() == "ACME");
+
+        Assert.assertTrue(testJob.getLocation() instanceof Location);
+        Assert.assertTrue(testJob.getLocation().getValue() == "Desert");
+
+        Assert.assertTrue(testJob.getPositionType() instanceof PositionType);
+        Assert.assertTrue(testJob.getPositionType().getValue() == "Quality control");
+
+        Assert.assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+        Assert.assertTrue(testJob.getCoreCompetency().getValue() == "Persistence");
+
+    }
+
+    @Test
+    public void testToStringStartsAndEndsWithNewLine()
+    {
+
+    }
 }
